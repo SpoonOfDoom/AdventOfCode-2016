f2ab57907d5554730fce4ccb33c3198c6345a136
Christian Syska <syska.christian@gmail.com>
2016-12-02 08:56:13 +0100

    Day 2 input and class

---
+++ AdventOfCode2016/Days/Day2.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2016.Days
+{
+	class Day2 : Day
+	{
+		public Day2() : base(2) {}
+		
+
+		public override string GetSolutionPart1()
+		{
+			return base.GetSolutionPart1();
+		}
+
+		public override string GetSolutionPart2()
+		{
+			return base.GetSolutionPart2();
+		}
+	}
+}
