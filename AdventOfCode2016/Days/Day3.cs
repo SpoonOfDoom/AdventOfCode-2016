3edaafe81c8dac13ba1575b156ddca64b8e9f16b
Christian Syska <belua85@gmail.com>
2016-12-04 19:13:46 +0100

    day 3 input and class

---
+++ AdventOfCode2016/Days/Day3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2016.Days
+{
+	class Day3 : Day
+	{
+		public Day3() : base(3) {}
+		
+
+		public override string GetSolutionPart1()
+		{
+			return base.GetSolutionPart1();
+		}
+
+		public override string GetSolutionPart2()
+		{
+			return base.GetSolutionPart2();
+		}
+	}
+}
--- AdventOfCode2016/Program.cs
+++ AdventOfCode2016/Program.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode2016
 			Stopwatch sw = new Stopwatch();
 			try
 			{
-				d = new Day2();
+				d = new Day3();
 			}
 			catch (Exception ex)
 			{
