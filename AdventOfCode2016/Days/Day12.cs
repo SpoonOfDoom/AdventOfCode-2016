944cc9dbe0f8ca8252a1bc33f45acde544ca6ec1
Christian Syska <syska.christian@gmail.com>
2016-12-12 09:35:02 +0100

    Day 12

--- AdventOfCode2016/Days/Day12.cs
+++ AdventOfCode2016/Days/Day12.cs
@@ -1,20 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AdventOfCode2016.Extensions;
 
 namespace AdventOfCode2016.Days
 {
 	class Day12 : Day
 	{
 		public Day12() : base(12) {}
+
+		private Dictionary<string, int> registers = new Dictionary<string, int>
+		{
+			{"a", 0 },
+			{"b", 0 },
+			{"c", 0 },
+			{"d", 0 },
+		};
+
+		private void ExecuteInstructions()
+		{
+			for (int index = 0; index < inputLines.Count; index++)
+			{
+				string line = inputLines[index];
+				var parts = line.Split(' ');
+				switch (parts[0])
+				{
+					case "cpy":
+						string v = parts[1];
+						int value = v.IsNumeric() ? v.ToInt() : registers[v];
+						string target = parts[2];
+						registers[target] = value;
+						break;
+
+					case "inc":
+						string incRegister = parts[1];
+						registers[incRegister]++;
+						break;
+
+					case "dec":
+						string decRegister = parts[1];
+						registers[decRegister]--;
+						break;
+
+					case "jnz":
+						int x = parts[1].IsNumeric()? parts[1].ToInt() : registers[parts[1]];
+						if (x != 0)
+						{
+							int amount = parts[2].ToInt();
+							index += amount -1;
+						}
+						break;
+				}
+			}
+		}
 		
 		public override string GetSolutionPart1()
 		{
-			return base.GetSolutionPart1();
+			ExecuteInstructions();
+			return registers["a"].ToString();
 		}
 
 		public override string GetSolutionPart2()
 		{
-			return base.GetSolutionPart2();
+			registers = new Dictionary<string, int>
+			{
+				{"a", 0 },
+				{"b", 0 },
+				{"c", 1 },
+				{"d", 0 },
+			};
+			ExecuteInstructions();
+			return  registers["a"].ToString();
 		}
 	}
 }
